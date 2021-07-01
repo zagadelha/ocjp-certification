4e918fbccd578c5e050540a9727f38f9706a6eab
Marcio Gadelha <zagadelha@gmail.com>
2021-06-30 22:58:26 -0300

    Adding sources about Java Arrays

---
+++ src/arrays/Array01.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class Array01 {
+
+	public static void main(String[] args) {
+
+		int[] prices;
+//		System.out.println(prices); // initialization error
+
+	}
+
+}
---
+++ src/arrays/Array02.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Array02 {
+
+	int[] prices;
+
+	public static void main(String[] args) {
+
+		Array02 a = new Array02();
+		System.out.println(a.prices); // works and print null
+
+	}
+
+}
